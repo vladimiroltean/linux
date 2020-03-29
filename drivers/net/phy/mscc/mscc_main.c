3c2088800e23e24c67f9611a091937a4a2c5f90d
Vladimir Oltean <vladimir.oltean@nxp.com>
2020-03-30 00:01:34 +0300

    net: phy: vsc8514: enable in-band SGMII auto-negotiation setting
    Rebasing this downstream patch on top of latest mscc phy driver.
    
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

--- drivers/net/phy/mscc/mscc_main.c
+++ drivers/net/phy/mscc/mscc_main.c
@@ -1570,6 +1570,7 @@ static int vsc8514_config_pre_init(struct phy_device *phydev)
 		{0x16b4, 0x00000814},
 	};
 	unsigned int i;
+	int ret;
 	u16 reg;
 
 	phy_base_write(phydev, MSCC_EXT_PAGE_ACCESS, MSCC_PHY_PAGE_STANDARD);
@@ -1595,6 +1596,15 @@ static int vsc8514_config_pre_init(struct phy_device *phydev)
 	reg = phy_base_read(phydev, MSCC_PHY_TEST_PAGE_8);
 	reg &= ~BIT(15);
 	phy_base_write(phydev, MSCC_PHY_TEST_PAGE_8, reg);
+
+	ret = phy_base_write(phydev, MSCC_EXT_PAGE_ACCESS,
+			     MSCC_PHY_PAGE_EXTENDED_3);
+	if (ret)
+		return ret;
+
+	ret = __phy_set_bits(phydev, MSCC_PHY_SERDES_CON, MSCC_PHY_SERDES_ANEG);
+	if (ret)
+		return ret;
 
 	phy_base_write(phydev, MSCC_EXT_PAGE_ACCESS, MSCC_PHY_PAGE_STANDARD);
 
@@ -1881,6 +1891,20 @@ static int vsc8514_config_init(struct phy_device *phydev)
 		phy_unlock_mdio_bus(phydev);
 		return -ETIMEDOUT;
 	}
+
+	ret = phy_base_write(phydev, MSCC_EXT_PAGE_ACCESS,
+			     MSCC_PHY_PAGE_EXTENDED_3);
+	if (ret)
+		goto err;
+
+	ret = __phy_set_bits(phydev, MSCC_PHY_SERDES_CON, MSCC_PHY_SERDES_ANEG);
+	if (ret)
+		goto err;
+
+	ret = phy_base_write(phydev, MSCC_EXT_PAGE_ACCESS,
+			     MSCC_PHY_PAGE_STANDARD);
+	if (ret)
+		goto err;
 
 	phy_unlock_mdio_bus(phydev);
 
