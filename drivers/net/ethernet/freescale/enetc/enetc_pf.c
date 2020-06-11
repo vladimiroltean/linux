04906df8dbe397e85943b7c45d00aff7450b9048
Po Liu <Po.Liu@nxp.com>
2020-06-11 13:50:24 +0800

    enetc: add support tsn capabilities qbv/qci/qbu/cbs
    Support Qbv/Qci/Qbu/Credit Base Shaper etc.
    This patch using the generic netlink adapt layer driver net/tsn/*
    and include/net/tsn.h interface load by user space. The user space
    refer the include/uapi/linux/tsn.h.
    
    Signed-off-by: Po Liu <Po.Liu@nxp.com>
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

--- drivers/net/ethernet/freescale/enetc/enetc_pf.c
+++ drivers/net/ethernet/freescale/enetc/enetc_pf.c
@@ -512,11 +512,13 @@ static void enetc_mac_config(struct enetc_hw *hw, phy_interface_t phy_mode)
 		val &= ENETC_PM0_IFM_IFMODE_MASK;
 		val |= ENETC_PM0_IFM_IFMODE_GMII | ENETC_PM0_IFM_RG;
 		enetc_port_wr(hw, ENETC_PM0_IF_MODE, val);
+		enetc_port_wr(hw, ENETC_PM1_IF_MODE, val);
 	}
 
 	if (phy_mode == PHY_INTERFACE_MODE_USXGMII) {
 		val = ENETC_PM0_IFM_FULL_DPX | ENETC_PM0_IFM_IFMODE_XGMII;
 		enetc_port_wr(hw, ENETC_PM0_IF_MODE, val);
+		enetc_port_wr(hw, ENETC_PM1_IF_MODE, val);
 	}
 }
 
@@ -748,6 +750,9 @@ static void enetc_pf_netdev_setup(struct enetc_si *si, struct net_device *ndev,
 		ndev->features |= NETIF_F_HW_TC;
 		ndev->hw_features |= NETIF_F_HW_TC;
 	}
+
+	if (enetc_tsn_is_enabled() && (si->hw_features & ENETC_SI_F_QBU))
+		priv->active_offloads |= ENETC_F_QBU;
 
 	/* pick up primary MAC address from SI */
 	enetc_get_primary_mac_addr(&si->hw, ndev->dev_addr);
@@ -1186,6 +1191,8 @@ static int enetc_pf_probe(struct pci_dev *pdev,
 	if (err)
 		goto err_reg_netdev;
 
+	enetc_tsn_pf_init(ndev, pdev);
+
 	return 0;
 
 err_reg_netdev:
@@ -1222,6 +1229,8 @@ static void enetc_pf_remove(struct pci_dev *pdev)
 
 	if (pf->num_vfs)
 		enetc_sriov_configure(pdev, 0);
+
+	enetc_tsn_pf_deinit(si->ndev);
 
 	unregister_netdev(si->ndev);
 
