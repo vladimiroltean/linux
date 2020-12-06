b1f00361721aacd32cce0c15daa777289666cbe9
Vladimir Oltean <vladimir.oltean@nxp.com>
2020-12-07 01:33:17 +0200

    net: mark ndo_get_stats64 as being able to sleep
    Now that all callers have been converted to not use atomic context when
    calling dev_get_stats, it is time to update the documentation and put a
    notice in the function that it expects process context.
    
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

--- net/core/dev.c
+++ net/core/dev.c
@@ -10408,6 +10408,8 @@ int dev_get_stats(struct net_device *dev, struct rtnl_link_stats64 *storage)
 	const struct net_device_ops *ops = dev->netdev_ops;
 	int err = 0;
 
+	might_sleep();
+
 	if (ops->ndo_get_stats64) {
 		memset(storage, 0, sizeof(*storage));
 		err = ops->ndo_get_stats64(dev, storage);
