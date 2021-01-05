9fe5dfe0f92a8465768a9b154c07ad1e242d12bc
Vladimir Oltean <vladimir.oltean@nxp.com>
2021-01-05 19:02:08 +0200

    net: remove return value from dev_get_stats
    After commit 28172739f0a2 ("net: fix 64 bit counters on 32 bit arches"),
    dev_get_stats got an additional argument for storage of statistics. At
    this point, dev_get_stats could return either the passed "storage"
    argument, or the output of .ndo_get_stats64.
    
    Then commit caf586e5f23c ("net: add a core netdev->rx_dropped counter")
    came, and the output of .ndo_get_stats64 (still returning a pointer to
    struct rtnl_link_stats64) started being ignored.
    
    Then came commit bc1f44709cf2 ("net: make ndo_get_stats64 a void
    function") which made .ndo_get_stats64 stop returning anything.
    
    So now, dev_get_stats always reports the "storage" pointer received as
    argument. This is useless. Some drivers are dealing with unnecessary
    complexity due to this, using another pointer to poke around the
    returned statistics, when they can do that directly through the
    stack-allocated struct rtnl_link_stats64.
    
    Refactor these callers to ignore the return value completely and just
    access the values from their struct rtnl_link_stats64 local variable.
    
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

--- net/core/net-procfs.c
+++ net/core/net-procfs.c
@@ -80,26 +80,27 @@ static void dev_seq_stop(struct seq_file *seq, void *v)
 
 static void dev_seq_printf_stats(struct seq_file *seq, struct net_device *dev)
 {
-	struct rtnl_link_stats64 temp;
-	const struct rtnl_link_stats64 *stats = dev_get_stats(dev, &temp);
+	struct rtnl_link_stats64 stats;
+
+	dev_get_stats(dev, &stats);
 
 	seq_printf(seq, "%6s: %7llu %7llu %4llu %4llu %4llu %5llu %10llu %9llu "
 		   "%8llu %7llu %4llu %4llu %4llu %5llu %7llu %10llu\n",
-		   dev->name, stats->rx_bytes, stats->rx_packets,
-		   stats->rx_errors,
-		   stats->rx_dropped + stats->rx_missed_errors,
-		   stats->rx_fifo_errors,
-		   stats->rx_length_errors + stats->rx_over_errors +
-		    stats->rx_crc_errors + stats->rx_frame_errors,
-		   stats->rx_compressed, stats->multicast,
-		   stats->tx_bytes, stats->tx_packets,
-		   stats->tx_errors, stats->tx_dropped,
-		   stats->tx_fifo_errors, stats->collisions,
-		   stats->tx_carrier_errors +
-		    stats->tx_aborted_errors +
-		    stats->tx_window_errors +
-		    stats->tx_heartbeat_errors,
-		   stats->tx_compressed);
+		   dev->name, stats.rx_bytes, stats.rx_packets,
+		   stats.rx_errors,
+		   stats.rx_dropped + stats.rx_missed_errors,
+		   stats.rx_fifo_errors,
+		   stats.rx_length_errors + stats.rx_over_errors +
+		    stats.rx_crc_errors + stats.rx_frame_errors,
+		   stats.rx_compressed, stats.multicast,
+		   stats.tx_bytes, stats.tx_packets,
+		   stats.tx_errors, stats.tx_dropped,
+		   stats.tx_fifo_errors, stats.collisions,
+		   stats.tx_carrier_errors +
+		    stats.tx_aborted_errors +
+		    stats.tx_window_errors +
+		    stats.tx_heartbeat_errors,
+		   stats.tx_compressed);
 }
 
 /*
