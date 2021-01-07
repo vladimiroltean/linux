b80907be9529d7e5aedadd7aa3c39f52bd7cc369
Vladimir Oltean <vladimir.oltean@nxp.com>
2021-01-07 22:33:15 +0200

    net: terminate errors from dev_get_stats
    dev_get_stats can now return errors. Some call sites are coming from a
    context that returns void (ethtool stats, workqueue context). So since
    we can't report to the upper layer, do the next best thing: shout.
    
    This patch wraps up the conversion of existing dev_get_stats callers, so
    we can add the __must_check attribute now to ensure that future callers
    keep doing this too.
    
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

--- arch/s390/appldata/appldata_net_sum.c
+++ arch/s390/appldata/appldata_net_sum.c
@@ -58,11 +58,11 @@ struct appldata_net_sum_data {
  */
 static void appldata_get_net_sum_data(void *data)
 {
-	int i;
 	struct appldata_net_sum_data *net_data;
 	struct net_device *dev;
 	unsigned long rx_packets, tx_packets, rx_bytes, tx_bytes, rx_errors,
 			tx_errors, rx_dropped, tx_dropped, collisions;
+	int ret, i;
 
 	net_data = data;
 	net_data->sync_count_1++;
@@ -83,7 +83,13 @@ static void appldata_get_net_sum_data(void *data)
 	for_each_netdev(&init_net, dev) {
 		struct rtnl_link_stats64 stats;
 
-		dev_get_stats(dev, &stats);
+		ret = dev_get_stats(dev, &stats);
+		if (ret) {
+			netif_lists_unlock(&init_net);
+			netdev_err(dev, "dev_get_stats returned %d\n", ret);
+			return;
+		}
+
 		rx_packets += stats.rx_packets;
 		tx_packets += stats.tx_packets;
 		rx_bytes   += stats.rx_bytes;
