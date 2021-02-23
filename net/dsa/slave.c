58d090887fae5ba89d2bd004bc63af12c365d7f1
Vladimir Oltean <vladimir.oltean@nxp.com>
2021-02-23 22:39:07 +0200

    net: dsa: replay port and local fdb entries when joining the bridge
    When a DSA port joins a LAG that already had an FDB entry pointing to it:
    
    ip link set bond0 master br0
    bridge fdb add dev bond0 00:01:02:03:04:05 master static
    ip link set swp0 master bond0
    
    the DSA port will have no idea that this FDB entry is there, because it
    missed the switchdev event emitted at its creation.
    
    Ido Schimmel pointed this out during a discussion about challenges with
    switchdev offloading of stacked interfaces between the physical port and
    the bridge, and recommended to just catch that condition and deny the
    CHANGEUPPER event:
    https://lore.kernel.org/netdev/20210210105949.GB287766@shredder.lan/
    
    But in fact, we might need to deal with the hard thing anyway, which is
    to replay all FDB addresses relevant to this port, because it isn't just
    static FDB entries, but also local addresses (ones that are not
    forwarded but terminated by the bridge). There, we can't just say 'oh
    yeah, there was an upper already so I'm not joining that'.
    
    So, similar to the logic for replaying MDB entries, add a function that
    must be called by individual switchdev drivers and replays local FDB
    entries as well as ones pointing towards a bridge port. This time, we
    use the atomic switchdev notifier block, since that's what FDB entries
    expect for some reason.
    
    Reported-by: Ido Schimmel <idosch@idosch.org>
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

--- net/dsa/slave.c
+++ net/dsa/slave.c
@@ -2290,7 +2290,8 @@ bool dsa_slave_dev_check(const struct net_device *dev)
 }
 EXPORT_SYMBOL_GPL(dsa_slave_dev_check);
 
-/* Circular reference */
+/* Circular references */
+static struct notifier_block dsa_slave_switchdev_notifier;
 static struct notifier_block dsa_slave_switchdev_blocking_notifier;
 
 static int dsa_slave_changeupper(struct net_device *dev,
@@ -2306,6 +2307,8 @@ static int dsa_slave_changeupper(struct net_device *dev,
 			err = dsa_port_bridge_join(dp, bridge_dev);
 			if (!err) {
 				dsa_bridge_mtu_normalization(dp);
+				br_fdb_replay(bridge_dev, dev,
+					      &dsa_slave_switchdev_notifier);
 				br_mdb_replay(bridge_dev, dev,
 					      &dsa_slave_switchdev_blocking_notifier);
 			}
@@ -2370,6 +2373,8 @@ dsa_slave_lag_changeupper(struct net_device *dev,
 	}
 
 	if (info->linking && netif_is_bridge_master(info->upper_dev) && !err) {
+		br_fdb_replay(info->upper_dev, dev,
+			      &dsa_slave_switchdev_notifier);
 		br_mdb_replay(info->upper_dev, dev,
 			      &dsa_slave_switchdev_blocking_notifier);
 	}
