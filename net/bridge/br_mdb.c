608f97585bb25ff85a926b14554f37f92e2bf4c2
Vladimir Oltean <vladimir.oltean@nxp.com>
2021-02-23 21:45:08 +0200

    net: dsa: replay port and host-joined mdb entries when joining the bridge
    I have udhcpcd in my system and this is configured to bring interfaces
    up as soon as they are created.
    
    I create a bridge as follows:
    
    ip link add br0 type bridge
    
    As soon as I create the bridge and udhcpcd brings it up, I have some
    other crap (avahi) that starts sending some random IPv6 packets to
    advertise some local services, and from there, the br0 bridge joins the
    following IPv6 groups:
    
    33:33:ff:6d:c1:9c vid 0
    33:33:00:00:00:6a vid 0
    33:33:00:00:00:fb vid 0
    
    br_dev_xmit
    -> br_multicast_rcv
       -> br_ip6_multicast_add_group
          -> __br_multicast_add_group
             -> br_multicast_host_join
                -> br_mdb_notify
    
    This is all fine, but inside br_mdb_notify we have br_mdb_switchdev_host
    hooked up, and switchdev will attempt to offload the host joined groups
    to an empty list of ports. Of course nobody offloads them.
    
    Then when we add a port to br0:
    
    ip link set swp0 master br0
    
    the bridge doesn't replay the host-joined MDB entries from br_add_if,
    and eventually the host joined addresses expire, and a switchdev
    notification for deleting it is emitted, but surprise, the original
    addition was already completely missed.
    
    The strategy to address this problem is to replay the MDB entries (both
    the port ones and the host joined ones) when the new port joins the
    bridge, similar to what vxlan_fdb_replay does (in that case, its FDB can
    be populated and only then attached to a bridge that you offload).
    However there are 2 possibilities: the addresses can be 'pushed' by the
    bridge into the port, or the port can 'pull' them from the bridge.
    
    Considering that in the general case, the new port can be really late to
    the party, and there may have been many other switchdev ports that
    already received the initial notification, we would like to avoid
    delivering duplicate events to them, since they might misbehave. And
    currently, the bridge calls the entire switchdev notifier chain, whereas
    for replaying it should just call the notifier block of the new guy.
    But the bridge doesn't know what is the new guy's notifier block, it
    just knows where the switchdev notifier chain is. So for simplification,
    we make this a driver-initiated pull for now, and the notifier block is
    passed as an argument.
    
    To emulate the calling context for mdb objects (deferred and put on the
    blocking notifier chain), we must iterate under RCU protection through
    the bridge's mdb entries, queue them, and only call them once we're out
    of the RCU read-side critical section.
    
    Suggested-by: Ido Schimmel <idosch@idosch.org>
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

--- net/bridge/br_mdb.c
+++ net/bridge/br_mdb.c
@@ -506,6 +506,123 @@ err:
 	kfree(priv);
 }
 
+static int br_mdb_replay_one(struct notifier_block *nb, struct net_device *dev,
+			     struct switchdev_obj_port_mdb *mdb)
+{
+	struct switchdev_notifier_port_obj_info obj_info = {
+		.info = {
+			.dev = dev,
+		},
+		.obj = &mdb->obj,
+	};
+	int err;
+
+	err = nb->notifier_call(nb, SWITCHDEV_PORT_OBJ_ADD, &obj_info);
+	return notifier_to_errno(err);
+}
+
+static int br_mdb_queue_one(struct list_head *mdb_list,
+			    enum switchdev_obj_id id,
+			    struct net_bridge_mdb_entry *mp,
+			    struct net_device *orig_dev)
+{
+	struct switchdev_obj_port_mdb *mdb;
+
+	mdb = kzalloc(sizeof(*mdb), GFP_ATOMIC);
+	if (!mdb)
+		return -ENOMEM;
+
+	mdb->obj.id = id;
+	mdb->obj.orig_dev = orig_dev;
+	mdb->vid = mp->addr.vid;
+
+	if (mp->addr.proto == htons(ETH_P_IP))
+		ip_eth_mc_map(mp->addr.dst.ip4, mdb->addr);
+#if IS_ENABLED(CONFIG_IPV6)
+	else if (mp->addr.proto == htons(ETH_P_IPV6))
+		ipv6_eth_mc_map(&mp->addr.dst.ip6, mdb->addr);
+#endif
+	else
+		ether_addr_copy(mdb->addr, mp->addr.dst.mac_addr);
+
+	list_add_tail(&mdb->obj.list, mdb_list);
+
+	return 0;
+}
+
+int br_mdb_replay(struct net_device *br_dev, struct net_device *dev,
+		  struct notifier_block *nb)
+{
+	struct net_bridge_mdb_entry *mp;
+	struct switchdev_obj *obj, *tmp;
+	struct list_head mdb_list;
+	struct net_bridge *br;
+	int err = 0;
+
+	ASSERT_RTNL();
+
+	INIT_LIST_HEAD(&mdb_list);
+
+	if (!netif_is_bridge_master(br_dev))
+		return -EINVAL;
+
+	if (!netif_is_bridge_port(dev))
+		return -EINVAL;
+
+	br = netdev_priv(br_dev);
+
+	if (!br_opt_get(br, BROPT_MULTICAST_ENABLED))
+		return 0;
+
+	rcu_read_lock();
+
+	hlist_for_each_entry_rcu(mp, &br->mdb_list, mdb_node) {
+		struct net_bridge_port_group __rcu **pp;
+		struct net_bridge_port_group *p;
+
+		if (mp->host_joined) {
+			err = br_mdb_queue_one(&mdb_list,
+					       SWITCHDEV_OBJ_ID_HOST_MDB,
+					       mp, br_dev);
+			if (err) {
+				rcu_read_unlock();
+				goto out_free_mdb;
+			}
+		}
+
+		for (pp = &mp->ports; (p = rcu_dereference(*pp)) != NULL;
+		     pp = &p->next) {
+			if (p->key.port->dev != dev)
+				continue;
+
+			err = br_mdb_queue_one(&mdb_list,
+					       SWITCHDEV_OBJ_ID_PORT_MDB,
+					       mp, dev);
+			if (err) {
+				rcu_read_unlock();
+				goto out_free_mdb;
+			}
+		}
+	}
+
+	rcu_read_unlock();
+
+	list_for_each_entry(obj, &mdb_list, list) {
+		err = br_mdb_replay_one(nb, dev, SWITCHDEV_OBJ_PORT_MDB(obj));
+		if (err)
+			goto out_free_mdb;
+	}
+
+out_free_mdb:
+	list_for_each_entry_safe(obj, tmp, &mdb_list, list) {
+		list_del(&obj->list);
+		kfree(SWITCHDEV_OBJ_PORT_MDB(obj));
+	}
+
+	return err;
+}
+EXPORT_SYMBOL(br_mdb_replay);
+
 static void br_mdb_switchdev_host_port(struct net_device *dev,
 				       struct net_device *lower_dev,
 				       struct net_bridge_mdb_entry *mp,
