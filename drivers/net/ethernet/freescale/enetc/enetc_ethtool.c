69a1cd618002c3b6c22bb0f944debb65c5f860d8
Vladimir Oltean <vladimir.oltean@nxp.com>
2021-03-09 13:37:19 +0200

    net: enetc: honor the min_frag_size frame preemption parameter
    Using our downstream ethtool patch, it is possible to specify a
    minFragSize value:
    
    ethtool --set-frame-preemption eno0 fp on preemptible-queues-mask 0x5e min-frag-size 124
    
    Currently the driver ignores the min-frag-size option and keeps it at
    60, the hardware default value.
    
    In hardware there are two fields in the Merge Control and Status
    Register (MAC_MERGE_MMCSR):
    
    LAFS (Local additional fragment size) holds the minFragSize required by
    our RX side. It is not used by hardware and just some storage in case
    the LLDP daemon wants to create an Additional Ethernet Capabilities TLV.
    Frankly this is useless.
    
    RAFS (Remote additional fragment size) holds the minFragSize required by
    the RX of the link partner. Hardware uses this to adjust the minFragSize
    on TX. This is what needs to be programmed.
    
    We also change the .get_preempt method to retrieve the RAFS register.
    
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

--- drivers/net/ethernet/freescale/enetc/enetc_ethtool.c
+++ drivers/net/ethernet/freescale/enetc/enetc_ethtool.c
@@ -745,10 +745,16 @@ static int enetc_set_preempt(struct net_device *ndev, struct ethtool_fp *pt)
 {
 	struct enetc_ndev_priv *priv = netdev_priv(ndev);
 	u32 preempt, temp;
+	int rafs;
 	int i;
 
 	if (!pt)
 		return -EINVAL;
+
+	if (pt->min_frag_size < 60 || pt->min_frag_size > 252)
+		return -EINVAL;
+
+	rafs = DIV_ROUND_UP((pt->min_frag_size + 4), 64) - 1;
 
 	if (!pt->fp_enabled)
 		preempt = 0x0;
@@ -774,6 +780,11 @@ static int enetc_set_preempt(struct net_device *ndev, struct ethtool_fp *pt)
 				      temp & ~ENETC_FPE);
 	}
 
+	temp = enetc_port_rd(&priv->si->hw, ENETC_MMCSR);
+	temp &= ~ENETC_MMCSR_RAFS_MASK;
+	temp |= ENETC_MMCSR_RAFS(rafs);
+	enetc_port_wr(&priv->si->hw, ENETC_MMCSR, temp);
+
 	return 0;
 }
 
@@ -798,8 +809,8 @@ static int enetc_get_preempt(struct net_device *ndev, struct ethtool_fp *pt)
 
 	pt->fp_supported = !!(priv->si->hw_features & ENETC_SI_F_QBU);
 	pt->supported_queues_mask = 0xff;
-	temp = (enetc_port_rd(&priv->si->hw, ENETC_MMCSR) & 0x18) >> 3;
-	pt->min_frag_size = (temp + 1) * 64;
+	temp = enetc_port_rd(&priv->si->hw, ENETC_MMCSR);
+	pt->min_frag_size = (ENETC_MMCSR_GET_RAFS(temp) + 1) * 64;
 
 	return 0;
 }
