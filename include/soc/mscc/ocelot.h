da0314652e9fcbc09aaf384fff570922cde22de3
Vladimir Oltean <vladimir.oltean@nxp.com>
2021-03-19 01:36:36 +0200

    net: ocelot: support multiple bridges
    The ocelot switches are a bit odd in that they do not have an STP state
    to put the ports into. Instead, the forwarding configuration is delayed
    from the typical port_bridge_join into stp_state_set, when the port enters
    the BR_STATE_FORWARDING state.
    
    I can only guess that the implementation of this quirk is the reason that
    led to the simplification of the driver such that only one bridge could
    be offloaded at a time.
    
    We can simplify the data structures somewhat, and introduce a per-port
    bridge device pointer and STP state, similar to how the LAG offload
    works now (there we have a per-port bonding device pointer and TX
    enabled state). This allows offloading multiple bridges with relative
    ease, while still keeping in place the quirk to delay the programming of
    the PGIDs.
    
    We actually need this change now because we need to remove the bogus
    restriction from ocelot_bridge_stp_state_set that ocelot->bridge_mask
    needs to contain BIT(port), otherwise that function is a no-op.
    
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit df291e54ccca0ef357f07a7b89263f7918d6ed7a)
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>
    (conflicts in drivers/net/ethernet/mscc/ocelot.c with commit
     421741ea5672 net: mscc: ocelot: offload bridge port flags to device
     and in include/soc/mscc/ocelot.h with commit
     7c588c3e96e9 net: ocelot: Extend MRP)

--- include/soc/mscc/ocelot.h
+++ include/soc/mscc/ocelot.h
@@ -608,6 +608,9 @@ struct ocelot_port {
 
 	struct net_device		*bond;
 	bool				lag_tx_active;
+
+	struct net_device		*bridge;
+	u8				stp_state;
 };
 
 struct ocelot {
@@ -626,10 +629,6 @@ struct ocelot {
 	int				packet_buffer_size;
 	int				num_frame_refs;
 	int				num_mact_rows;
-
-	struct net_device		*hw_bridge_dev;
-	u16				bridge_mask;
-	u16				bridge_fwd_mask;
 
 	struct ocelot_port		**ports;
 
