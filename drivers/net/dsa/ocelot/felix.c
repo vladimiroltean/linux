777c7debf7fdcf8666a2927b67935362425b61e1
Yangbo Lu <yangbo.lu@nxp.com>
2021-04-27 12:22:00 +0800

    net: dsa: free skb->cb usage in core driver
    Free skb->cb usage in core driver and let device drivers decide to
    use or not. The reason having a DSA_SKB_CB(skb)->clone was because
    dsa_skb_tx_timestamp() which may set the clone pointer was called
    before p->xmit() which would use the clone if any, and the device
    driver has no way to initialize the clone pointer.
    
    This patch just put memset(skb->cb, 0, sizeof(skb->cb)) at beginning
    of dsa_slave_xmit(). Some new features in the future, like one-step
    timestamp may need more bytes of skb->cb to use in
    dsa_skb_tx_timestamp(), and p->xmit().
    
    Signed-off-by: Yangbo Lu <yangbo.lu@nxp.com>
    Acked-by: Richard Cochran <richardcochran@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit c4b364ce1270d689ee5010001344b8eae3685f32)
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

--- drivers/net/dsa/ocelot/felix.c
+++ drivers/net/dsa/ocelot/felix.c
@@ -1396,7 +1396,7 @@ static void felix_txtstamp(struct dsa_switch *ds, int port,
 			return;
 
 		ocelot_port_add_txtstamp_skb(ocelot, port, clone);
-		DSA_SKB_CB(skb)->clone = clone;
+		OCELOT_SKB_CB(skb)->clone = clone;
 	}
 }
 
