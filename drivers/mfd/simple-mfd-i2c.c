7d639fa2212a6e6a206bf7e97371640410e919b5
Vladimir Oltean <vladimir.oltean@nxp.com>
2021-11-24 11:38:01 +0200

    mfd: simple-mfd-i2c: add compatible string for LS1028A-QDS FPGA
    As Michael mentions in the description of commit 3abee4579484 ("mfd: Add
    simple regmap based I2C driver"), "If a device wants to use this as its
    MFD core driver, it has to add an individual compatible string."
    
    The QIXIS FPGA on the LS1028A-QDS boards has a similar purpose to the
    Kontron SL28 CPLD: it deals with board power-on reset timing, muxing,
    etc.
    
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

--- drivers/mfd/simple-mfd-i2c.c
+++ drivers/mfd/simple-mfd-i2c.c
@@ -84,6 +84,7 @@ static const struct simple_mfd_data maxim_max5970 = {
 };
 
 static const struct of_device_id simple_mfd_i2c_of_match[] = {
+	{ .compatible = "fsl,ls1028a-qds-qixis-i2c" },
 	{ .compatible = "kontron,sl28cpld" },
 	{ .compatible = "silergy,sy7636a", .data = &silergy_sy7636a},
 	{ .compatible = "maxim,max5970", .data = &maxim_max5970},
