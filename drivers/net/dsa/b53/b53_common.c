664f992656adb08d6108c939a421c31d276f5bea
Vladimir Oltean <vladimir.oltean@nxp.com>
2021-12-06 18:57:56 +0200

    net: dsa: keep the bridge_dev and bridge_num as part of the same structure
    The main desire behind this is to provide coherent bridge information to
    the fast path without locking.
    
    For example, right now we set dp->bridge_dev and dp->bridge_num from
    separate code paths, it is theoretically possible for a packet
    transmission to read these two port properties consecutively and find a
    bridge number which does not correspond with the bridge device.
    
    Another desire is to start passing more complex bridge information to
    dsa_switch_ops functions. For example, with FDB isolation, it is
    expected that drivers will need to be passed the bridge which requested
    an FDB/MDB entry to be offloaded, and along with that bridge_dev, the
    associated bridge_num should be passed too, in case the driver might
    want to implement an isolation scheme based on that number.
    
    We already pass the {bridge_dev, bridge_num} pair to the TX forwarding
    offload switch API, however we'd like to remove that and squash it into
    the basic bridge join/leave API. So that means we need to pass this
    pair to the bridge join/leave API.
    
    During dsa_port_bridge_leave, first we unset dp->bridge_dev, then we
    call the driver's .port_bridge_leave with what used to be our
    dp->bridge_dev, but provided as an argument.
    
    When bridge_dev and bridge_num get folded into a single structure, we
    need to preserve this behavior in dsa_port_bridge_leave: we need a copy
    of what used to be in dp->bridge.
    
    Switch drivers check bridge membership by comparing dp->bridge_dev with
    the provided bridge_dev, but now, if we provide the struct dsa_bridge as
    a pointer, they cannot keep comparing dp->bridge to the provided
    pointer, since this only points to an on-stack copy. To make this
    obvious and prevent driver writers from forgetting and doing stupid
    things, in this new API, the struct dsa_bridge is provided as a full
    structure (not very large, contains an int and a pointer) instead of a
    pointer. An explicit comparison function needs to be used to determine
    bridge membership: dsa_port_offloads_bridge().
    
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>
    Reviewed-by: Alvin Šipraga <alsi@bang-olufsen.dk>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- drivers/net/dsa/b53/b53_common.c
+++ drivers/net/dsa/b53/b53_common.c
@@ -1874,7 +1874,7 @@ int b53_mdb_del(struct dsa_switch *ds, int port,
 }
 EXPORT_SYMBOL(b53_mdb_del);
 
-int b53_br_join(struct dsa_switch *ds, int port, struct net_device *br)
+int b53_br_join(struct dsa_switch *ds, int port, struct dsa_bridge bridge)
 {
 	struct b53_device *dev = ds->priv;
 	s8 cpu_port = dsa_to_port(ds, port)->cpu_dp->index;
@@ -1901,7 +1901,7 @@ int b53_br_join(struct dsa_switch *ds, int port, struct net_device *br)
 	b53_read16(dev, B53_PVLAN_PAGE, B53_PVLAN_PORT_MASK(port), &pvlan);
 
 	b53_for_each_port(dev, i) {
-		if (dsa_port_bridge_dev_get(dsa_to_port(ds, i)) != br)
+		if (!dsa_port_offloads_bridge(dsa_to_port(ds, i), &bridge))
 			continue;
 
 		/* Add this local port to the remote port VLAN control
@@ -1925,7 +1925,7 @@ int b53_br_join(struct dsa_switch *ds, int port, struct net_device *br)
 }
 EXPORT_SYMBOL(b53_br_join);
 
-void b53_br_leave(struct dsa_switch *ds, int port, struct net_device *br)
+void b53_br_leave(struct dsa_switch *ds, int port, struct dsa_bridge bridge)
 {
 	struct b53_device *dev = ds->priv;
 	struct b53_vlan *vl = &dev->vlans[0];
@@ -1937,7 +1937,7 @@ void b53_br_leave(struct dsa_switch *ds, int port, struct net_device *br)
 
 	b53_for_each_port(dev, i) {
 		/* Don't touch the remaining ports */
-		if (dsa_port_bridge_dev_get(dsa_to_port(ds, i)) != br)
+		if (!dsa_port_offloads_bridge(dsa_to_port(ds, i), &bridge))
 			continue;
 
 		b53_read16(dev, B53_PVLAN_PAGE, B53_PVLAN_PORT_MASK(i), &reg);
