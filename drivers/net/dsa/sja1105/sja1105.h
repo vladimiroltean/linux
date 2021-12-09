1a6ff8e522e89fe1f111f02b0145f88019b1a16e
Vladimir Oltean <vladimir.oltean@nxp.com>
2021-12-10 01:34:41 +0200

    net: dsa: sja1105: remove hwts_tx_en from tagger data
    This tagger property is in fact not used at all by the tagger, only by
    the switch driver. Therefore it makes sense to be moved to
    sja1105_private.
    
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/dsa/sja1105/sja1105.h
+++ drivers/net/dsa/sja1105/sja1105.h
@@ -250,6 +250,7 @@ struct sja1105_private {
 	bool vlan_aware;
 	unsigned long ucast_egress_floods;
 	unsigned long bcast_egress_floods;
+	unsigned long hwts_tx_en;
 	const struct sja1105_info *info;
 	size_t max_xfer_len;
 	struct gpio_desc *reset_gpio;
