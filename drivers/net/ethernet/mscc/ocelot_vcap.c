7fa5775e1ab9b14e227ab865e385732daa8c38bb
Vladimir Oltean <vladimir.oltean@nxp.com>
2022-02-16 16:30:10 +0200

    net: mscc: ocelot: keep traps in a list
    When using the ocelot-8021q tagging protocol, the CPU port isn't
    configured as an NPI port, but is a regular port. So a "trap to CPU"
    operation is actually a "redirect" operation. So DSA needs to set up the
    trapping action one way or another, depending on the tagging protocol in
    use.
    
    To ease DSA's work of modifying the action, keep all currently installed
    traps in a list, so that DSA can live-patch them when the tagging
    protocol changes.
    
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/mscc/ocelot_vcap.c
+++ drivers/net/ethernet/mscc/ocelot_vcap.c
@@ -1408,6 +1408,7 @@ int ocelot_vcap_init(struct ocelot *ocelot)
 	}
 
 	INIT_LIST_HEAD(&ocelot->dummy_rules);
+	INIT_LIST_HEAD(&ocelot->traps);
 	INIT_LIST_HEAD(&ocelot->vcap_pol.pol_list);
 
 	return 0;
