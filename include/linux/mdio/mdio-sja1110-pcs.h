0b648917d9d420addfe51ecbec6fc179e5332c1e
Vladimir Oltean <vladimir.oltean@nxp.com>
2023-02-21 23:04:19 +0200

    net: mdio: add driver for SJA1110 PCS access
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

---
+++ include/linux/mdio/mdio-sja1110-pcs.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: (GPL-2.0 OR MIT) */
+/* Copyright 2022 NXP
+ */
+#ifndef MDIO_NXP_SJA1110_PCS_H
+#define MDIO_NXP_SJA1110_PCS_H
+
+#define MDIO_SJA1110_PCS_NAME	"sja1110-pcs-mdio"
+
+#endif
