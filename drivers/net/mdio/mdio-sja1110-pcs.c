25811a0e534363297e0827b045efb438f5efa56b
Vladimir Oltean <vladimir.oltean@nxp.com>
2023-02-21 23:04:19 +0200

    net: mdio: add driver for SJA1110 PCS access
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

---
+++ drivers/net/mdio/mdio-sja1110-pcs.c
@@ -0,0 +1,140 @@
+// SPDX-License-Identifier: GPL-2.0
+/* Copyright 2022 NXP
+ *
+ * NXP SJA1110 PCS MDIO bus driver
+ */
+#include <linux/mdio/mdio-sja1110-pcs.h>
+#include <linux/module.h>
+#include <linux/of_mdio.h>
+#include <linux/pcs/pcs-xpcs.h>
+#include <linux/phy.h>
+#include <linux/platform_device.h>
+#include <linux/regmap.h>
+
+#define SJA1110_PCS_BANK_REG		0xff0
+
+static int sja1110_pcs_mdio_read_c45(struct mii_bus *bus, int phy, int mmd,
+				     int reg)
+{
+	struct regmap *regmap = bus->priv;
+	u64 addr = (mmd << 16) | reg;
+	int offset, bank;
+	u32 val;
+	int err;
+
+	if (mmd == MDIO_MMD_VEND2 && (reg & GENMASK(15, 0)) == MII_PHYSID1)
+		return NXP_SJA1110_XPCS_ID >> 16;
+	if (mmd == MDIO_MMD_VEND2 && (reg & GENMASK(15, 0)) == MII_PHYSID2)
+		return NXP_SJA1110_XPCS_ID & GENMASK(15, 0);
+
+	bank = addr >> 8;
+	offset = addr & GENMASK(7, 0);
+
+	/* This addressing scheme reserves register 0xff for the bank address
+	 * register, so that can never be addressed.
+	 */
+	if (WARN_ON(offset == 0xff))
+		return -ENODEV;
+
+	err = regmap_write(regmap, SJA1110_PCS_BANK_REG, bank);
+	if (err)
+		return err;
+
+	err = regmap_read(regmap, offset, &val);
+	if (err)
+		return err;
+
+	return val & 0xffff;
+}
+
+static int sja1110_pcs_mdio_write_c45(struct mii_bus *bus, int phy, int reg,
+				      int mmd, u16 val)
+{
+	struct regmap *regmap = bus->priv;
+	u64 addr = (mmd << 16) | reg;
+	int offset, bank;
+	int err;
+
+	bank = addr >> 8;
+	offset = addr & GENMASK(7, 0);
+
+	/* This addressing scheme reserves register 0xff for the bank address
+	 * register, so that can never be addressed.
+	 */
+	if (WARN_ON(offset == 0xff))
+		return -ENODEV;
+
+	err = regmap_write(regmap, SJA1110_PCS_BANK_REG, bank);
+	if (err)
+		return err;
+
+	return regmap_write(regmap, offset, val);
+}
+
+static int sja1110_pcs_mdio_probe(struct platform_device *pdev)
+{
+	struct device *dev = &pdev->dev;
+	struct regmap *regmap;
+	struct resource *res;
+	struct mii_bus *bus;
+	int err;
+
+	if (!dev->of_node || !dev->parent)
+		return -ENODEV;
+
+	res = platform_get_resource(pdev, IORESOURCE_REG, 0);
+	if (!res)
+		return -ENODEV;
+
+	regmap = dev_get_regmap(dev->parent, res->name);
+	if (!regmap)
+		return -ENODEV;
+
+	bus = mdiobus_alloc_size(0);
+	if (!bus)
+		return -ENOMEM;
+
+	bus->name = "SJA1110 PCS MDIO bus";
+	snprintf(bus->id, MII_BUS_ID_SIZE, "%s", dev_name(dev));
+	bus->read_c45 = sja1110_pcs_mdio_read_c45;
+	bus->write_c45 = sja1110_pcs_mdio_write_c45;
+	bus->parent = dev;
+	bus->priv = regmap;
+
+	err = of_mdiobus_register(bus, dev->of_node);
+	if (err)
+		goto err_free_bus;
+
+	platform_set_drvdata(pdev, bus);
+
+	return 0;
+
+err_free_bus:
+	mdiobus_free(bus);
+
+	return err;
+}
+
+static int sja1110_pcs_mdio_remove(struct platform_device *pdev)
+{
+	struct mii_bus *bus = platform_get_drvdata(pdev);
+
+	mdiobus_unregister(bus);
+	mdiobus_free(bus);
+
+	return 0;
+}
+
+static struct platform_driver sja1110_pcs_mdio_driver = {
+	.probe = sja1110_pcs_mdio_probe,
+	.remove = sja1110_pcs_mdio_remove,
+	.driver = {
+		.name = MDIO_SJA1110_PCS_NAME,
+	},
+};
+
+module_platform_driver(sja1110_pcs_mdio_driver);
+
+MODULE_DESCRIPTION("NXP SJA1110 PCS MDIO bus driver");
+MODULE_AUTHOR("Vladimir Oltean <vladimir.oltean@nxp.com>");
+MODULE_LICENSE("GPL");
