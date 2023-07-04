5a108b53d439e8def65d523ead5de82950ebde58
Vladimir Oltean <vladimir.oltean@nxp.com>
2023-07-05 01:51:11 +0300

    net: remove phy_has_hwtstamp() -> phy_mii_ioctl() decision from converted drivers
    It is desirable that the new .ndo_hwtstamp_set() API gives more
    uniformity, less overhead and future flexibility w.r.t. the PHY
    timestamping behavior.
    
    Currently there are some drivers which allow PHY timestamping through
    the procedure mentioned in Documentation/networking/timestamping.rst.
    They don't do anything locally if phy_has_hwtstamp() is set, except for
    lan966x which installs PTP packet traps.
    
    Centralize that behavior in a new dev_set_hwtstamp_phylib() code
    function, which calls either phy_mii_ioctl() for the phylib PHY,
    or .ndo_hwtstamp_set() of the netdev, based on a single policy
    (currently simplistic: phy_has_hwtstamp()).
    
    Any driver converted to .ndo_hwtstamp_set() will automatically opt into
    the centralized phylib timestamping policy. Unconverted drivers still
    get to choose whether they let the PHY handle timestamping or not.
    
    Netdev drivers with integrated PHY drivers that don't use phylib
    presumably don't set dev->phydev, and those will always see
    HWTSTAMP_SOURCE_NETDEV requests even when converted. The timestamping
    policy will remain 100% up to them.
    
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

--- include/linux/net_tstamp.h
+++ include/linux/net_tstamp.h
@@ -5,6 +5,11 @@
 
 #include <uapi/linux/net_tstamp.h>
 
+enum hwtstamp_source {
+	HWTSTAMP_SOURCE_NETDEV,
+	HWTSTAMP_SOURCE_PHYLIB,
+};
+
 /**
  * struct kernel_hwtstamp_config - Kernel copy of struct hwtstamp_config
  *
@@ -26,6 +31,7 @@ struct kernel_hwtstamp_config {
 	int rx_filter;
 	struct ifreq *ifr;
 	bool copied_to_user;
+	enum hwtstamp_source source;
 };
 
 static inline void hwtstamp_config_to_kernel(struct kernel_hwtstamp_config *kernel_cfg,
@@ -42,6 +48,14 @@ static inline void hwtstamp_config_from_kernel(struct hwtstamp_config *cfg,
 	cfg->flags = kernel_cfg->flags;
 	cfg->tx_type = kernel_cfg->tx_type;
 	cfg->rx_filter = kernel_cfg->rx_filter;
+}
+
+static inline bool kernel_hwtstamp_config_changed(const struct kernel_hwtstamp_config *a,
+						  const struct kernel_hwtstamp_config *b)
+{
+	return a->flags != b->flags ||
+	       a->tx_type != b->tx_type ||
+	       a->rx_filter != b->rx_filter;
 }
 
 #endif /* _LINUX_NET_TIMESTAMPING_H_ */
