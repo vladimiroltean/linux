415eef158df3bd4e1e998f236851bad7788d2412
Vladimir Oltean <vladimir.oltean@nxp.com>
2023-07-04 21:38:23 +0300

    net: enetc: convert to ndo_hwtstamp_get() and ndo_hwtstamp_set()
    The hardware timestamping through ndo_eth_ioctl() is going away.
    Convert the enetc driver to the new API before that can be removed.
    
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

--- drivers/net/ethernet/freescale/enetc/enetc_pf.c
+++ drivers/net/ethernet/freescale/enetc/enetc_pf.c
@@ -773,6 +773,8 @@ static const struct net_device_ops enetc_ndev_ops = {
 	.ndo_setup_tc		= enetc_pf_setup_tc,
 	.ndo_bpf		= enetc_setup_bpf,
 	.ndo_xdp_xmit		= enetc_xdp_xmit,
+	.ndo_hwtstamp_get	= enetc_hwtstamp_get,
+	.ndo_hwtstamp_set	= enetc_hwtstamp_set,
 };
 
 static void enetc_pf_netdev_setup(struct enetc_si *si, struct net_device *ndev,
