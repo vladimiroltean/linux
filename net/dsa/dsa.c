412cf8fa07c75e19db31951bd4c5569cacb074a5
Vladimir Oltean <vladimir.oltean@nxp.com>
2025-02-19 12:30:47 +0200

    net: dsa: use of_property_present() instead of of_property_read_bool()
    The "link" phandle is not a boolean property, we just want to see
    whether is present, so use the correct OF API for that.
    
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

--- net/dsa/dsa.c
+++ net/dsa/dsa.c
@@ -1240,7 +1240,7 @@ static int dsa_port_parse_of(struct dsa_port *dp, struct device_node *dn)
 {
 	struct device_node *ethernet = of_parse_phandle(dn, "ethernet", 0);
 	const char *name = of_get_property(dn, "label", NULL);
-	bool link = of_property_read_bool(dn, "link");
+	bool link = of_property_present(dn, "link");
 
 	dp->dn = dn;
 
