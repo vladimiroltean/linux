d4c486ebd51182cefcab4c8f90653ac866b57049
Vladimir Oltean <vladimir.oltean@nxp.com>
2026-02-24 15:18:38 +0200

    mmc: sdhci-of-arasan: deprecate <linux/phy/phy.h>
    The sdhci-of-arasan driver is a PHY consumer. Include the specific
    <linux/phy/phy-consumer.h> which does not give access to the struct phy
    guts.
    
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

--- drivers/mmc/host/sdhci-of-arasan.c
+++ drivers/mmc/host/sdhci-of-arasan.c
@@ -20,7 +20,7 @@
 #include <linux/module.h>
 #include <linux/of.h>
 #include <linux/platform_device.h>
-#include <linux/phy/phy.h>
+#include <linux/phy/phy-consumer.h>
 #include <linux/regmap.h>
 #include <linux/reset.h>
 #include <linux/firmware/xlnx-zynqmp.h>
