b6612002ce84e385688c8ac71478f8c2ff219ac8
Vladimir Oltean <vladimir.oltean@nxp.com>
2026-02-24 12:52:15 +0200

    phy: add <linux/pm_runtime.h> where missing
    It appears that the phy-mapphone-mdm6600, phy-qcom-snps-femto-v2,
    phy-rcar-gen3-pcie, r8a779f0-ether-serdes and phy-rockchip-typec drivers
    call runtime PM operations without including the proper header.
    
    This was provided by <linux/phy/phy.h> but no function exported by this
    header directly needs it. So we need to drop it from there, and fix up
    drivers that used to depend on that.
    
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>
    Reviewed-by: Geert Uytterhoeven <geert+renesas@glider.be> # renesas
    Reviewed-by: André Draszik <andre.draszik@linaro.org> # google

--- drivers/phy/rockchip/phy-rockchip-typec.c
+++ drivers/phy/rockchip/phy-rockchip-typec.c
@@ -49,6 +49,7 @@
 #include <linux/of_address.h>
 #include <linux/of_platform.h>
 #include <linux/platform_device.h>
+#include <linux/pm_runtime.h>
 #include <linux/regmap.h>
 #include <linux/reset.h>
 
