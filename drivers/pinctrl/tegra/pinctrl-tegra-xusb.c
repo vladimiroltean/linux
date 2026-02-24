59dd00007e61ce05e79b5069f8e40c873d686ce0
Vladimir Oltean <vladimir.oltean@nxp.com>
2026-02-24 15:20:08 +0200

    pinctrl: tegra-xusb: deprecate <linux/phy/phy.h>
    The tegra-xusb pinctrl driver is also a PHY provider (calls
    devm_phy_create() for PCIe and SATA).
    
    Mark that correctly by using the <linux/phy/phy-provider.h> header,
    which also gives hints about what is permissible from an API
    perspective. The classic <linux/phy/phy.h> is going to be removed.
    
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

--- drivers/pinctrl/tegra/pinctrl-tegra-xusb.c
+++ drivers/pinctrl/tegra/pinctrl-tegra-xusb.c
@@ -7,7 +7,7 @@
 #include <linux/io.h>
 #include <linux/module.h>
 #include <linux/of.h>
-#include <linux/phy/phy.h>
+#include <linux/phy/phy-provider.h>
 #include <linux/platform_device.h>
 #include <linux/reset.h>
 #include <linux/seq_file.h>
